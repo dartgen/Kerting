ace82bddae3959d19427bac54761cd2e84d68f36
totoka28 <totoka282@gmail.com>
2026-03-29 18:52:36 +0200

    Profil uploder hiba javitva!

--- backend/Kerting_Api/Controller/UserController.cs
+++ backend/Kerting_Api/Controller/UserController.cs
@@ -139,7 +139,7 @@ namespace Kerting_Api.Controller
             var roles = await _context.Role.ToListAsync();
             return Ok(roles);
         }
-        [AllowAnonymous]
+        [Authorize]
         [HttpGet("GetPublicProfile/{id}")]
         // Ez a végpont publikus, NEM kell rá [Authorize]!
         public async Task<IActionResult> GetPublicProfile(int id)
